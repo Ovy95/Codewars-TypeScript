6b57529fe05fed29e07b0ffeb3ff290bf1c58c6b
Jack Overton <jackoverton21@hotmail.com>
2023-07-26 12:03:53 +0100

    Returns words of 5 characters or more correct way round

--- 6 Kyu/StopgninnipSMysdroW!/src/spinWords.test.ts
+++ 6 Kyu/StopgninnipSMysdroW!/src/spinWords.test.ts
@@ -3,3 +3,11 @@ import spinWords from './spinWords';
 test('Basic test two characters long should return value', () => {
   expect(spinWords('hi')).toBe('hi');
 });
+
+test('Returns Hello when given olleH', () => {
+  expect(spinWords('olleH')).toBe('Hello');
+});
+
+test('Returns Hello hows it going', () => {
+  expect(spinWords('olleH hows it gniog')).toBe('Hello hows it going');
+});
--- 6 Kyu/StopgninnipSMysdroW!/src/spinWords.ts
+++ 6 Kyu/StopgninnipSMysdroW!/src/spinWords.ts
@@ -1,5 +1,14 @@
 function spinWords(words: string): string {
-  return words;
+  const wordsArray = words.split(' ');
+
+  for (let i = 0; i < wordsArray.length; i++) {
+    const word = wordsArray[i];
+    if (word.length > 4) {
+      wordsArray[i] = word.split('').reverse().join('');
+    }
+  }
+
+  return wordsArray.join(' ');
 }
 
 export default spinWords;
